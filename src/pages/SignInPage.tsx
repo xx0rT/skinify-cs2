c0b2dad422adc6ed64bcce8d817f9f8688e1ffa5
Troxx <txrxo.troxx@gmail.com>
2026-07-12 18:34:55 +0200

    u

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import {
